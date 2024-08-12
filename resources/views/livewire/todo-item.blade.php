7299e78eee098b187eb529d814fd026be7e74928
MASHINJIGODFREY <gmashinji@gmail.com>
2024-08-12 09:34:33 +0300

    Added Loading Animations

--- resources/views/livewire/todo-item.blade.php
+++ resources/views/livewire/todo-item.blade.php
@@ -9,11 +9,13 @@
         @else
         <span class="ballotbox badge bg-warning pt-2 pb-2 pl-2 pr-2"> Pending</span>
         <button type="button" wire:click="markAsComplete" class="btn btn-primary btn-sm">
-            <span class="checkmark"></span> Mark as Complete
+            <span class="checkmark" wire:loading.remove> Mark as Done</span>
+            <span class="hourglass" wire:loading wire:target="markAsComplete"> Processing...</span>
         </button>
         @endif
         <button type="button" class="btn btn-danger btn-sm" wire:click="delete" wire:confirm="Are you sure you want to delete this item?">
-            <span class="crossmark"></span> Delete
+            <span class="crossmark" wire:loading.remove> Delete</span>
+            <span class="hourglass" wire:loading wire:target="delete"> Deleting...</span>
         </button>
     </div>
 </li>
